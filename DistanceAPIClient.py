c58c59bcc788acfdfda0d9fc3b4c307df94c9852
František Hluchník <frantahluchnik@gmail.com>
2023-03-20 13:41:54 +0100

    prototype

--- DistanceAPIClient.py
+++ DistanceAPIClient.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, random
 
 class DistanceAPIClient:
     def __init__(self, api_key):
@@ -7,16 +7,18 @@ class DistanceAPIClient:
     def get_path(self, lat1, lon1, lat2, lon2):
         req_url_prep = 'https://api.openrouteservice.org/v2/directions/{type}?api_key={key}&start={start_lon},{start_lat}&end={end_lon},{end_lat}'
         req_url = req_url_prep.format(type = 'foot-walking', key = self.api_key, start_lat = lat1, start_lon = lon1, end_lat = lat2, end_lon = lon2)
-        print(req_url)
         try:
             response = requests.get(req_url)
         except requests.exceptions.RequestException as e:
             print(response.text)
             raise SrystemExit(e)
-        print(response.text)
+        # print(response.text)
         return json.loads(response.text)
 
-    def test_get_path():
+    def test_get_path(self):
         with open('test_response.json') as test_response:
             parsed_json = json.load(test_response)
         return parsed_json
+
+    def random_path(self):
+        return random.randrange(1, 10, 1)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from openrouteservice import DistanceAPIClient
-from pathfinder import ShortestPath
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+import DistanceAPIClient
+import pathfinder
+# from dotenv import load_dotenv
+# import os
+#
+# load_dotenv()
 
 locations = [
     (49.206677, 16.605728),
@@ -13,10 +13,24 @@ locations = [
     (49.204382, 16.606715),
 ]
 
-distance_api = DistanceAPIClient(os.getenv("API_KEY"))
-shortest_path = ShortestPath(locations, distance_api)
+# locations = [
+#     (1, 1),
+#     (2, 2),
+#     (3, 3),
+#     (4, 4),
+#     (5, 5),
+# ]
+
+graph = pathfinder.create_graph(locations)
+res = pathfinder.brute_force_tsp(graph)
+print(res)
+
+
+
+# distance_api = DistanceAPIClient(os.getenv("API_KEY"))
+# shortest_path = ShortestPath(locations, distance_api)
 # path = shortest_path.get_shortest_path()
-path = distance_api.get_path(49.2016811, 16.6106769, 49.2049758, 16.6133592)
-p = path['features'][0]['properties']['segments'][0]['distance']
-print(p)
+# path = distance_api.test_get_path(49.2016811, 16.6106769, 49.2049758, 16.6133592)
+# p = path['features'][0]['properties']['segments'][0]['distance']
+# print(p)
 # print(path)
